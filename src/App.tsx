181bf2b11cee76e3fab5b17b527323907e0bc1b4
Navyanth03 <mekalanavyanthyadav2003@gmail.com>
2024-08-19 02:07:25 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App=()=>{
+  return (
+    <div className="flex min-h-screen justify-center  items-center">
+      <div className="text-4xl">Hi this is Navyanth M</div>
+    </div>
+  )
+}
+
+export default App;
