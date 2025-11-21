78196c9a2823d6d382ee3fc97a5fb6669602e3a0
navyanth04 <mekalanavyanthyadav2003@gmail.com>
2025-11-20 20:25:25 -0500

    Add leetcode link

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -46,58 +46,7 @@ export default function Component() {
                     translateZ={20}
                     translateX={40}
                     as="a"
-                    href="#"
-                    className="flex items-center px-4 py-2 text-white text-xl font-bold"
-                  >
-                    <GitHubLogoIcon className="w-8 h-8" />
-                  </CardItem>
-                </div>
-              </CardBody>
-            </CardContainer>
-            <CardContainer className="inter-var">
-              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                <CardItem
-                  translateZ="60"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
-                >
-                  Classroom Management - for seamless management.
-                </CardItem>
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                >
-                  TypeScript | React | Node | Prisma | Tailwind | MySQL
-                </CardItem>
-                <CardItem
-                  translateZ="100"
-                  rotateX={0}
-                  rotateZ={-3}
-                  className="w-full mt-4"
-                >
-                  <Image
-                    src="/assests/classroom_management.jpg"
-                    height="1000"
-                    width="1000"
-                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                    alt="thumbnail"
-                  />
-                </CardItem>
-                <div className="flex justify-between items-center mt-20">
-                  <CardItem
-                    translateZ={20}
-                    translateX={-40}
-                    as="a"
-                    href="https://classroommanagementclient-navyanth-ms-projects.vercel.app/landing"
-                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                  >
-                    Live Link →
-                  </CardItem>
-                  <CardItem
-                    translateZ={20}
-                    translateX={40}
-                    as="a"
-                    href="https://github.com/Navyanth03/classroom_management"
+                    href="https://github.com/Navyanth03/PayFlow"
                     className="flex items-center px-4 py-2 text-white text-xl font-bold"
                   >
                     <GitHubLogoIcon className="w-8 h-8" />
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,21 +1,27 @@
-"use client"
-'use client'
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import { useTheme } from 'next-themes'; // Assuming Next.js for theme switching
-import { Mail, Github, Linkedin } from 'lucide-react';
+"use client";
+"use client";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { useTheme } from "next-themes"; // Assuming Next.js for theme switching
+import { Mail, Github, Linkedin } from "lucide-react";
 
 const ContactMe: React.FC = () => {
   const { theme } = useTheme(); // Next.js theme switcher hook
-  const [mounted,setMounted]=useState(false);
+  const [mounted, setMounted] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     setMounted(true);
-  },[])
+  }, []);
 
-  if(!mounted)return null;
+  if (!mounted) return null;
   return (
-    <div className={`flex flex-col items-center justify-center h-[24rem] ${theme === 'dark' ? 'bg-transparent  text-white' : 'bg-transparent text-black'}`}>
+    <div
+      className={`flex flex-col items-center justify-center h-[24rem] ${
+        theme === "dark"
+          ? "bg-transparent  text-white"
+          : "bg-transparent text-black"
+      }`}
+    >
       <motion.h1
         className=" mb-4 text-black dark:text-white "
         initial={{ opacity: 0, y: -50 }}
@@ -34,7 +40,6 @@ const ContactMe: React.FC = () => {
         <motion.a
           href="mailto:mekalanavyanthyadav2003@gmail.com"
           className="flex flex-col items-center p-4 bg-transparent dark:bg-transparent dark:text-white rounded-lg border border-black dark:border-white shadow-sm hover:bg-gray-200 dark:hover:bg-gray-600 transition transform hover:scale-105 hover:rotate-3"
-
           whileHover={{ scale: 1.1, rotate: 5 }}
         >
           <Mail className="h-8 w-8 mb-2 text-red-800" />
@@ -44,7 +49,6 @@ const ContactMe: React.FC = () => {
         <motion.a
           href="https://github.com/Navyanth03/"
           className="flex flex-col items-center p-4 bg-transparent dark:bg-transparent dark:text-white dar rounded-lg border border-black dark:border-white shadow-sm hover:bg-gray-200 dark:hover:bg-gray-600 transition transform hover:scale-105 hover:rotate-3"
-
           whileHover={{ scale: 1.1, rotate: 5 }}
         >
           <Github className="h-8 w-8 mb-2 text-gray-700" />
@@ -52,9 +56,30 @@ const ContactMe: React.FC = () => {
         </motion.a>
 
         <motion.a
-          href="https://www.linkedin.com/in/m-navyanth-bb475b288/"
-          className="flex flex-col items-center p-4 bg-transparent dark:bg-transparent dark:text-white rounded-lg border border-black dark:border-white shadow-sm hover:bg-gray-200 dark:hover:bg-gray-600 transition transform hover:scale-105 hover:rotate-3"
+          href="https://github.com/Navyanth04/"
+          className="flex flex-col items-center p-4 bg-transparent dark:bg-transparent dark:text-white dar rounded-lg border border-black dark:border-white shadow-sm hover:bg-gray-200 dark:hover:bg-gray-600 transition transform hover:scale-105 hover:rotate-3"
+          whileHover={{ scale: 1.1, rotate: 5 }}
+        >
+          <Github className="h-8 w-8 mb-2 text-gray-700" />
+          <span>GitHub</span>
+        </motion.a>
 
+        <motion.a
+          href="https://leetcode.com/u/mekalanavyanthyadav2003/"
+          className="flex flex-col items-center p-4 bg-transparent dark:bg-transparent dark:text-white rounded-lg border border-black dark:border-white shadow-sm hover:bg-gray-200 dark:hover:bg-gray-600 transition transform hover:scale-105 hover:rotate-3"
+          whileHover={{ scale: 1.1, rotate: 5 }}
+        >
+          <img
+            src="/assets/leetcode.png"
+            alt="LeetCode"
+            className="h-8 w-8 mb-2 object-contain"
+          />
+          <span>LeetCode</span>
+        </motion.a>
+
+        <motion.a
+          href="https://www.linkedin.com/in/navyanth-mekala-bb475b288"
+          className="flex flex-col items-center p-4 bg-transparent dark:bg-transparent dark:text-white rounded-lg border border-black dark:border-white shadow-sm hover:bg-gray-200 dark:hover:bg-gray-600 transition transform hover:scale-105 hover:rotate-3"
           whileHover={{ scale: 1.1, rotate: -5 }}
         >
           <Linkedin className="h-8 w-8 mb-2 text-blue-700" />
